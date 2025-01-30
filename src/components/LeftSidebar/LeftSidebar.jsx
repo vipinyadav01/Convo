1abca8f869746eee609ee867019cc1139d30271b
vipinyadav01 <vipinyadav9m@gmail.com>
2025-01-30 23:27:08 +0530

    style: enhance LeftSidebar and add RightSidebar components with styling and layout

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import "./LeftSidebar.css"
-import assets from '../../assets/assets'
+import React from 'react';
+import "./LeftSidebar.css";
+import assets from '../../assets/assets';
 
 const LeftSidebar = () => {
     return (
         <div className='ls'>
             <div className='ls-top'>
                 <div className="ls-nav">
-                    <img src={assets.logo} className='logo' alt="" />
+                    <img src={assets.logo} className='logo' alt="Logo" />
                     <div className="menu">
-                        <img src={assets.menu_icon} alt="" />
+                        <img src={assets.menu_icon} alt="Menu Icon" />
+                        <div className="sub-menu">
+                            <p>Edit Profile</p>
+                            <br />
+                            <p>Logout</p>
+                        </div>
                     </div>
                 </div>
                 <div className="ls-search">
-                    <img src={assets.search_icon} alt="" />
+                    <img src={assets.search_icon} alt="Search Icon" />
                     <input type="text" placeholder="Search or start new chat" />
                 </div>
             </div>
             <div className="ls-list">
                 {Array(12).fill("").map((item, index) => (
                     <div key={index} className="friends">
-                        <img src={assets.profile_img} alt="" />
+                        <img src={assets.profile_img} alt="Profile" />
                         <div>
                             <p>Vipin Yadav</p>
                             <span>Hello,How are you?</span>
@@ -32,4 +37,4 @@ const LeftSidebar = () => {
     )
 }
 
-export default LeftSidebar
+export default LeftSidebar;
--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import "./RightSidebar.css"
+import assets from "../../assets/assets"
 
 const RightSidebar = () => {
     return (
-        <div>
-
+        <div className='rs'>
+            <div className="rs-profile">
+                <img src={assets.profile_img} alt="" />
+                <h3>Vipin Yadav <img src={assets.green_dot} className='dot' alt='' /></h3>
+                <p>Hey, I am Vipin Yadav. I'm using the chat app Convo, and I am a Software Developer.</p>
+            </div>
+            <hr />
+            <div className='rs-media'>
+                <p>Media</p>
+                <div>
+                    <img src={assets.pic1} alt="" />
+                    <img src={assets.pic2} alt="" />
+                    <img src={assets.pic3} alt="" />
+                    <img src={assets.pic4} alt="" />
+                    <img src={assets.pic1} alt="" />
+                    <img src={assets.pic2} alt="" />
+                </div>
+            </div>
+            <button>Logout</button>
         </div>
     )
 }

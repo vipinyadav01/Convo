6d1ef05f9cb6873876ba5360a2075f23708e03bb
vipinyadav01 <vipinyadav9m@gmail.com>
2025-02-03 22:07:50 +0530

    fix: update favicon path and set default chat visibility to true

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -12,7 +12,7 @@ const AppContextProvider = (props) => {
     const [messagesId, setMessagesId] = useState(null);
     const [messages, setMessages] = useState([]);
     const [chatUser, setChatUser] = useState(null);
-    const [chatVisible, setChatVisible] = useState(false);
+    const [chatVisible, setChatVisible] = useState(true);
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((user) => {
